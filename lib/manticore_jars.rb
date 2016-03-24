bfb85d3ebd67d9681c911718adec8d9a42fbce8a
Chris Heald <cheald@gmail.com>
2016-03-24 15:28:43 -0700

    Specify version ranges for commons libs, as well

--- lib/manticore_jars.rb
+++ lib/manticore_jars.rb
@@ -2,7 +2,7 @@
 require 'jar_dependencies'
 
 require_jar( 'commons-logging', 'commons-logging', '1.2' )
-require_jar( 'commons-codec', 'commons-codec', '1.9' )
 require_jar( 'org.apache.httpcomponents', 'httpmime', '4.5.2' )
+require_jar( 'commons-codec', 'commons-codec', '1.10' )
 require_jar( 'org.apache.httpcomponents', 'httpclient', '4.5.2' )
 require_jar( 'org.apache.httpcomponents', 'httpcore', '4.4.4' )
