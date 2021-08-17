902015c669c63baac8a5133af094f81749ee4224
kaisecheng <69120390+kaisecheng@users.noreply.github.com>
2021-08-17 21:45:13 +0200

    set socket config to connection manager (#98)

--- lib/manticore/client.rb
+++ lib/manticore/client.rb
@@ -203,11 +203,7 @@ module Manticore
         builder.set_connection_reuse_strategy DefaultConnectionReuseStrategy.new
       end
 
-      socket_config_builder = SocketConfig.custom
-      socket_config_builder.set_so_timeout(options.fetch(:socket_timeout, DEFAULT_SOCKET_TIMEOUT) * 1000)
-      socket_config_builder.set_tcp_no_delay(options.fetch(:tcp_no_delay, true))
-      builder.set_default_socket_config socket_config_builder.build
-
+      builder.set_default_socket_config socket_config_from_options(options)
       builder.set_connection_manager pool(options)
 
       request_config = RequestConfig.custom
@@ -409,9 +405,18 @@ module Manticore
           cm.set_validate_after_inactivity options.fetch(:check_connection_timeout, 2_000)
           cm.set_default_max_per_route options.fetch(:pool_max_per_route, @max_pool_size)
           cm.set_max_total @max_pool_size
+          cm.set_default_socket_config socket_config_from_options(options)
+
           finalize cm, :shutdown
         end
       end
+    end
+    
+    def socket_config_from_options(options)
+      socket_config_builder = SocketConfig.custom
+      socket_config_builder.set_so_timeout(options.fetch(:socket_timeout, DEFAULT_SOCKET_TIMEOUT) * 1000)
+      socket_config_builder.set_tcp_no_delay(options.fetch(:tcp_no_delay, true))
+      socket_config_builder.build
     end
 
     def create_executor_if_needed
