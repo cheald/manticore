ffcdac59764f5779d1e2e9ecd78ec62c83120152
Karol Bucek <kares@users.noreply.github.com>
2021-12-21 19:00:34 +0100

    [deps] update http-client to 4.5.13 (#99)
    * [deps] update http-client to 4.5.13
    
    * [build] recompiled ext (using 4.5.13)

--- lib/manticore_jars.rb
+++ lib/manticore_jars.rb
@@ -1,8 +1,18 @@
 # this is a generated file, to avoid over-writing it just delete this comment
-require "jar_dependencies"
+begin
+  require 'jar_dependencies'
+rescue LoadError
+  require 'commons-logging/commons-logging/1.2/commons-logging-1.2.jar'
+  require 'commons-codec/commons-codec/1.15/commons-codec-1.15.jar'
+  require 'org/apache/httpcomponents/httpcore/4.4.14/httpcore-4.4.14.jar'
+  require 'org/apache/httpcomponents/httpclient/4.5.13/httpclient-4.5.13.jar'
+  require 'org/apache/httpcomponents/httpmime/4.5.13/httpmime-4.5.13.jar'
+end
 
-require_jar("commons-logging", "commons-logging", "1.2")
-require_jar("org.apache.httpcomponents", "httpmime", "4.5.2")
-require_jar("commons-codec", "commons-codec", "1.10")
-require_jar("org.apache.httpcomponents", "httpclient", "4.5.2")
-require_jar("org.apache.httpcomponents", "httpcore", "4.4.4")
+if defined? Jars
+  require_jar 'commons-logging', 'commons-logging', '1.2'
+  require_jar 'commons-codec', 'commons-codec', '1.15'
+  require_jar 'org.apache.httpcomponents', 'httpcore', '4.4.14'
+  require_jar 'org.apache.httpcomponents', 'httpclient', '4.5.13'
+  require_jar 'org.apache.httpcomponents', 'httpmime', '4.5.13'
+end
